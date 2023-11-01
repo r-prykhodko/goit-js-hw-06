ac17027714be84e309f8046a54a3ce8a123b6dc1
Roman Prykhodko <r.prykhodko@yahoo.com>
2023-11-01 16:05:20 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,33 @@
+
+const ingredientsList = document.querySelector("#ingredients");
+
+
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatoes",
+  "Herbs",
+  "Condiments",
 ];
+
+
+const fragment = document.createDocumentFragment();
+
+
+ingredients.forEach((ingredientText) => {
+
+  const ingredientItem = document.createElement("li");
+
+
+  ingredientItem.textContent = ingredientText;
+
+
+  ingredientItem.classList.add("item");
+
+
+  fragment.appendChild(ingredientItem);
+});
+
+
+ingredientsList.appendChild(fragment);
