df51486c9ce48fdc14ed536e69be885541c6a44d
Roman Prykhodko <r.prykhodko@yahoo.com>
2023-11-01 16:22:48 +0200

    3

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,26 @@
 
+const loginForm = document.querySelector(".login-form");
+
+loginForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const formData = new FormData(loginForm);
+
+    const userData = {};
+
+    formData.forEach((value, key) => {
+        userData[key] = value;
+    });
+
+
+    if (!userData.email || !userData.password) {
+        alert("Всі поля повинні бути заповнені!");
+        return;
+    }
+
+
+    console.log(userData);
+
+
+    loginForm.reset();
+});
