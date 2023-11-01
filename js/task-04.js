22305cef82d57acb0714d6e7190291536e18185c
Roman Prykhodko <r.prykhodko@yahoo.com>
2023-11-01 16:10:56 +0200

    Update task-04.js

---
+++ js/task-04.js
@@ -0,0 +1,24 @@
+const counter = document.querySelector("#counter");
+const valueSpan = document.querySelector("#value");
+const incrementButton = counter.querySelector('[data-action="increment"]');
+const decrementButton = counter.querySelector('[data-action="decrement"]');
+
+
+let counterValue = 0;
+
+
+incrementButton.addEventListener("click", () => {
+    counterValue += 1;
+    updateCounterValue();
+});
+
+
+decrementButton.addEventListener("click", () => {
+    counterValue -= 1;
+    updateCounterValue();
+});
+
+
+function updateCounterValue() {
+    valueSpan.textContent = counterValue;
+}
