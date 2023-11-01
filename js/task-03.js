21daa181466b551133dc0358a2bdf39686c0390c
Roman Prykhodko <r.prykhodko@yahoo.com>
2023-11-01 16:08:39 +0200

    Update task-03.js

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,16 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+
+const galleryList = document.querySelector(".gallery");
+
+
+const galleryItemsHTML = images.map((image) => `
+  <li class="gallery-item">
+    <img src="${image.url}" alt="${image.alt}" class="gallery-image">
+  </li>
+`).join('');
+
+
+galleryList.insertAdjacentHTML("beforeend", galleryItemsHTML);
